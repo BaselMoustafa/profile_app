62629e4dd5b205c276f83bd6ba2a35b9b14f7388
Basel Moustafa <basel.moustafa2000@gmail.com>
2025-08-07 21:15:01 +0300

    add single child scroll view

--- lib/profile_screen.dart
+++ lib/profile_screen.dart
@@ -17,56 +17,72 @@ class ProfileScreen extends StatelessWidget {
             padding:EdgeInsetsDirectional.symmetric(
               horizontal: 20
             ),
-            child: Column(
-              spacing: 10,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-            
-                SizedBox(height: 35,),
-            
-                ProfileImageWidget(),
-            
-                Text(
-                  "Basel Moustafa",
-                  style: TextStyle(
-                    fontSize: 35,
-                    fontWeight: FontWeight.w800
+            child: SingleChildScrollView(
+              child: Column(
+                spacing: 10,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+              
+                  SizedBox(height: 35,),
+              
+                  ProfileImageWidget(),
+              
+                  Text(
+                    "Basel Moustafa",
+                    style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.w800
+                    ),
                   ),
-                ),
-            
-                Text(
-                  "Flutter Developer",
-                  style: TextStyle(
-                    color: AppColors.primary,
-                    fontSize: 18
+              
+                  Text(
+                    "Flutter Developer",
+                    style: TextStyle(
+                      color: AppColors.primary,
+                      fontSize: 18
+                    ),
                   ),
-                ),
-            
-                Text(
-                  "Passionate Flutter developer building beautiful, high-performance mobile apps. Love creating smooth, user-focused experiences that make an impact.",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500
+              
+                  Text(
+                    "Passionate Flutter developer building beautiful, high-performance mobile apps. Love creating smooth, user-focused experiences that make an impact.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500
+                    ),
                   ),
-                ),
-
-                SizedBox(height: 10,),
-
-                SocialMediaContactsWidget(),
-
-
-                InfoCardWidget(
-                  color: Colors.deepPurple,
-                  iconData: Icons.work,
-                  title: "Experience",
-                  subtitle: "3 years of experience",
-                ),
-
-
-                
-              ],
+              
+                  SizedBox(height: 10,),
+              
+                  SocialMediaContactsWidget(),
+              
+                  SizedBox(height: 10,),
+              
+                  InfoCardWidget(
+                    color: Colors.deepPurple,
+                    iconData: Icons.work,
+                    title: "Experience",
+                    subtitle: "3 years of experience",
+                  ),
+              
+                  InfoCardWidget(
+                    color: Colors.pink,
+                    iconData: Icons.location_city,
+                    title: "Location",
+                    subtitle: "Alexandria, Egypt",
+                  ),
+              
+                  InfoCardWidget(
+                    color: Colors.greenAccent,
+                    iconData: Icons.calendar_view_day_rounded,
+                    title: "Joined",
+                    subtitle: "Jan 2025",
+                  ),
+              
+                  
+                ],
+              ),
             ),
           ),
         ),
